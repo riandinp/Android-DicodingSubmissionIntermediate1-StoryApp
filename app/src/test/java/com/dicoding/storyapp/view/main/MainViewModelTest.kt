2ea66063d62ca1351212b4f79dab798a64dbe41d
riandinp <riandinandaputra@yahoo.com>
2023-05-04 10:45:33 +0700

    fix unit testing

--- app/src/test/java/com/dicoding/storyapp/view/main/MainViewModelTest.kt
+++ app/src/test/java/com/dicoding/storyapp/view/main/MainViewModelTest.kt
@@ -59,7 +59,7 @@ class MainViewModelTest {
 
         Assert.assertNotNull(differ.snapshot())
         Assert.assertEquals(dummyStory.size, differ.snapshot().size)
-        Assert.assertEquals(dummyStory[0].id, differ.snapshot()[0]?.id)
+        Assert.assertEquals(dummyStory[0], differ.snapshot()[0])
     }
 
     @Test
